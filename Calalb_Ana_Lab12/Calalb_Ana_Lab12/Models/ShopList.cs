fcd0bd4a1d828911946400568f828f6c51466d86
Ana Calalb <calalb.ana@gmail.com>
2021-01-07 18:45:03 +0200

    Add project files.

---
+++ Calalb_Ana_Lab12/Calalb_Ana_Lab12/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calalb_Ana_Lab12.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
